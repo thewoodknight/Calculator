05bf99a095717ec78aa7ab0822b5c0772c5671d3
Paul Jenkins <paul@thewoodknight.com>
2021-08-18 08:59:42 +1000

    init

---
+++ Calculator/Pages/Bit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Calculator.Pages
+{
+    public class Bit
+    {
+        public string BitImperial { get; set; }
+        public double BitMetric { get; set; }
+        public List<PossibleCombination> Combinations { get; set; }
+    }
+}
---
+++ Calculator/Pages/Index.razor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace Calculator.Pages
+{
+    public partial class Index
+    {
+        private double[] CommonBits = new double[] { 3.125, 6.35, 9.5, 12.7 };
+        private double[] CommonBushings = new double[] { 7.95, 9.5, 11.13, 12.7, 15.875, 17, 19.01, 34.9 };
+        private double DesiredHole = 20;
+
+        /* RA1128 contains 5/16, 7/16, 5/8, 1-3/8, 1/2, 3/4 */
+        //  TemplateID = DesiredHole + (BushingOD - BitOD);
+
+        public List<Bit> Bits = new List<Bit>();
+
+        public void ConvertClick()
+        {
+            Bits = new List<Bit>();
+
+            foreach (var bit in CommonBits)
+            {
+                var b = new Bit()
+                {
+                    BitMetric = bit,
+                    BitImperial = FractionExtension.ToFraction(bit / 25.4),
+                    Combinations = new List<PossibleCombination>(),
+                };
+
+                foreach (var bushing in CommonBushings)
+                {
+                    if (bit < bushing && DesiredHole > bit)
+                    {
+                        var t = DesiredHole + (bushing - bit);
+                        b.Combinations.Add(new PossibleCombination(bushing, t));
+                    }
+                }
+
+                Bits.Add(b);
+            }
+        }
+    }
+}
